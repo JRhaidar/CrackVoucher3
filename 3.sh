2aecd5e7eb1a7843bd0348fdd6cdb53ea7b08dd3
LordKazuma <45675547+lordkazuma@users.noreply.github.com>
2019-04-28 22:28:02 +0700

    Add files via upload

---
+++ 3.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#Have Fun
+#Untuk Hiburan Semata
+clear
+echo "\033[32;1m
+████████╗██╗  ██╗██████╗ ███████╗███████╗
+╚══██╔══╝██║  ██║██╔══██╗██╔════╝██╔════╝
+   ██║   ███████║██████╔╝█████╗  █████╗  
+   ██║   ██╔══██║██╔══██╗██╔══╝  ██╔══╝  
+   ██║   ██║  ██║██║  ██║███████╗███████╗
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝
+                                         
+"
+
+echo "\033[32;1m_________________________________________"
+echo "\033[32;1m|\033[31;1mAuthor	 	:\033[32;1mLordKazuma		|"	
+echo "\033[32;1m|\033[31;1mWhatsapp 	:\033[32;1m0895706053139		|"
+echo "\033[32;1m|\033[31;1mFacebook 	:\033[32;1mUdiinn			|"
+echo "\033[32;1m________________________________________|"
+echo 
+echo "\033[31;1mCrack Voucher Pulsa Three"
+echo "\033[35;1mCara Pengaktifan Voucher *111*16(kode Voucher) Yes atau Ok"
+echo "\033[33;1mApakah Anda Ingin Melanjutkan?\033[31;1mY\033[32;1m/\033[31;1mN"
+read -p "Jawaban-->" "jwb"
+if [ $jwb = Y ] | [ $jwb = y ]; then
+	sleep 3
+	echo "\033[34;1m Sedang Mengambil data"
+	sleep 4
+	echo "\033[34;1m Memproses"
+	sleep 4
+	echo "\033[31;1m1%"
+	sleep 2
+	echo "\033[31;1m4%"
+	sleep 2
+	echo "\033[31;1m8%"
+	sleep 2
+	echo "\033[31;1m10%"
+	sleep 2
+	echo "\033[31;1m15%"
+	sleep 2
+	echo "\033[31;1m20%"
+	sleep 2
+	echo "\033[31;1m30%"
+	sleep 2
+	echo "\033[31;1m35%"
+	sleep 2
+	echo "\033[31;1m50%"
+	sleep 2
+	echo "\033[31;1m70%"
+	sleep 2
+	echo "\033[31;1m75%"
+	sleep 2
+	echo "\033[31;1m99%"
+	sleep 2
+	echo "\033[31;1m100%"
+	sleep 2
+	echo "\033[33;1mBerikut Ini Adalah Voucher Yang Berhasil DiDapatkan:"
+	echo 
+	echo "\033[34;1m5214451248244976"
+	echo
+	echo "\033[32;1mGunakan Dengan Bijak"
+	echo "\033[32;1mSemoga Harimu Menyenangkan"
+fi
+
+if [ $jwb = N ] | [ $jwb = n ]; then
+echo "\033[32;1m
+ _______  _______  __   __  _______  __    _  _______  ______    _______ 
+|       ||   _   ||  | |  ||       ||  |  | ||   _   ||    _ |  |   _   |
+|  _____||  |_|  ||  |_|  ||   _   ||   |_| ||  |_|  ||   | ||  |  |_|  |
+| |_____ |       ||       ||  | |  ||       ||       ||   |_||_ |       |
+|_____  ||       ||_     _||  |_|  ||  _    ||       ||    __  ||       |
+ _____| ||   _   |  |   |  |       || | |   ||   _   ||   |  | ||   _   |
+|_______||__| |__|  |___|  |_______||_|  |__||__| |__||___|  |_||__| |__|
+
+
+"
+fi
+echo "\033[0;1m"
